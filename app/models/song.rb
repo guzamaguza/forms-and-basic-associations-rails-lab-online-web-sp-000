38150981872a64b5f2f62034765604b13929d622
Mark A Guza <guzamaguza@gmail.com>
2020-08-06 00:44:47 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,23 +8,23 @@ class Song < ActiveRecord::Base
     def artist_name=(name)
       self.artist = Artist.find_or_create_by(name: name)
     end
-  
+
     def artist_name
       self.artist ? self.artist.name : ''
     end
-  
+
     def genre_name=(name)
       self.genre = Genre.find_or_create_by(name: name)
     end
-  
+
     def genre_name
       self.genre ? self.genre.name : ''
     end
-  
+
     def note_contents
       notes.collect {|note| note.content}
     end
-  
+
     def note_contents=(notes)
       notes.each do |content|
         note = self.notes.build(content: content) unless content.empty?
